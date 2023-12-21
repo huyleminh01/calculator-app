51a5819a934f9bff9f0b2e7dc2e38e9fd75ac2fb
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2023-12-21 14:22:05 +0700

    Calculation API

--- src/controllers/expression.controller.js
+++ src/controllers/expression.controller.js
@@ -10,9 +10,9 @@ import { ResponseBuilder } from "../common/utils/builders/response.builder";
 export function calculateExpression(req, res) {
     const { body } = req;
 
-    const { expression } = body;
+    const expression = body.expression || "";
 
     const result = new ExpressionBuilder(expression).build().calculate();
 
-    res.json(new ResponseBuilder().withCode(HTTP_CODE.created).withData(result).build());
+    res.json(new ResponseBuilder().withCode(HTTP_CODE.ok).withData({ result }).build());
 }
